41ddd4e48f46927b6802cfd67be263e8e7402092
Oussama Bengagi <89886406+Oussamabengagi@users.noreply.github.com>
2022-03-15 20:42:15 +0100

    Made some changes with index and added bootstrap

--- src/isetj/servlets/CompteurServlet.java
+++ src/isetj/servlets/CompteurServlet.java
@@ -45,9 +45,12 @@ public class CompteurServlet extends HttpServlet {
 	      out.println( 
 	         "<html>\n" +
 	            "<head><title> Compteur  </title></head>\n" +
+	        		 "<!-- CSS only -->\r\n"
+	        		 + "<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3\" crossorigin=\"anonymous\">"+
 	            "<body >\n" +
 	               "<h1 align = \"center\"> Total page Refreshs : </h1>\n" +
 	               "<h2 align = \"center\">" + hitCount + "</h2>\n" +
+	               "<a href= \"CompteurServlet\" class= \"btn btn-success text-center\" > Actualiser Cette Page</a>" +
 	            "</body> " +
 	        " </html>"
 	      );
